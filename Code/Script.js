a6f7fd9e57c5245e59558b2faf7e127b3af4dbde
Florin Mocanu <Mokanu@DESKTOP-BOGNLK2>
2018-06-02 12:57:29 +0200

    Inserting new element

--- Code/Script.js
+++ Code/Script.js
@@ -1,7 +1,26 @@
 
-$(function () {     
 
-    var $newitem = $('<li>Inception</li>');     
-    $('ol').append($newitem); 
 
-});
+  $(function () {
+
+    var $newitem=$("<li>Inception</li>");
+
+    // INSERTING ELEMENTS 
+
+    // If you like crazy things..
+    //$("li").prepend($newitem);
+
+    // Append the item at the end of the list
+    //$("ol").append($newitem);
+
+    // Prepend the item at the top of the list
+    //$("ol").prepend($newitem);
+
+    // Insert the item before the list, it's not part of the list
+    //$("ol").before($newitem);
+
+    // Insert the item after the list, it's not part of the list
+    //$("ol").after($newitem);
+
+
+  }); 
