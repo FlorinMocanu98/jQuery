3f863f01093e13daa77b29e3a749434d9ae7abc2
florin.mocanu <florin.mocanu@PC-STAGE03>
2018-06-07 17:06:08 +0200

    Changes

--- Code/jQuery/Effects.js
+++ Code/jQuery/Effects.js
@@ -1,12 +1,130 @@
 ﻿
 
-//
-
-
+// Example N1
+/*
 $("button:last").on("click", function () {
 
 
     $("button:first").toggle(600, "linear");
 
 
+}); */
+
+
+
+
+
+// Example N2
+/*
+$("button:last").on("click", function () {
+
+    $("button:first").fadeOut(600, function () {
+
+        $("button:first").css("background-color", "green").text("Hello Again").fadeIn(2000, function () {
+
+            $("button:last").text("Welcome back!").fadeIn(500);
+
+        });
+
+
+    });
+
 });
+
+*/
+
+
+
+
+
+// Example N3
+/*
+$("li").each(function () {
+
+
+    $(this).on("click", function () {
+
+
+        $(this).fadeTo(700, 0.25, function () {
+
+            $(this).append("&nbsp;&nbsp;&nbsp;OK!").css("color", "blue").fadeIn(2000, "swing")
+
+
+        });
+
+    });
+
+});
+
+*/
+
+
+
+// Example N4
+/*
+$("ol").on("click", function () {
+
+    $("ol").slideUp(1000, "swing", function () {
+
+        $("body").append("The list is disappeared!").css("font-size", "2vw");
+
+    });
+
+
+
+});
+*/
+
+
+
+// Example N5
+/*
+$("ol").hide();
+$("button").on("click", function () {
+
+
+    //if ($("ol").is(":hidden"))
+    $("ol").slideDown(600, "swing");
+ 
+
+
+});
+*/
+
+
+
+// Example N6
+/*
+$("button").on("click", function () {
+
+    $("ol").slideToggle(500, "linear");
+
+});
+*/
+
+
+
+
+
+
+// Example N7
+/*
+var count = 100;
+$("li").on("click", function () {
+
+    
+
+    $(this).animate({
+
+        fontSize: 60,
+        paddingLeft: count,
+        opacity: 0.4
+    }, 600, "linear");
+
+
+    count = count + 100;
+
+});
+*/
+
+
--- Code/jQuery/FindingElements.js
+++ Code/jQuery/FindingElements.js
@@ -2,24 +2,72 @@
 
 $(document).ready(function () {
 
+
     var $newitem = $("<li>Inception</li>");
     var $newitem2;
 
-  /* FINDING ELEMENTS BY ORDER */
+    /* FINDING ELEMENTS BY ORDER */
 
 
     // This finds all the items with the index greater than 2
-      //$("li:gt(2)").addClass("cool");
+    //$("li:gt(2)").addClass("cool");
 
 
 
     // This finds all the items with the index less than 2
-      //$("li:lt(2)").addClass("cool");
+    //$("li:lt(2)").addClass("cool");
+
 
 
     // And there is also eq() that specifies the index
-      //$("li").eq(0).addClass("cool");
+    //$("li").eq(0).addClass("cool");
+
+
 
     // This works the same as eq()
-      //$("first:nth-child(0)").addClass("cool");
-}); 
+    //$("first:nth-child(0)").addClass("cool");
+
+
+
+    // Add a new selector
+    //$("li:nth-child(1)").css({ "color": "blue" , "font-style" : "italic" }).add("li:nth-child(3)").css("font-size", "3vw");
+
+
+
+    // Another example
+    //$("li:first").css("color", "blue").add("li:nth-child(2)").css("font-size", "3vw").add("li:nth-child(3)").css("font-style", "italic");
+
+
+
+
+    // Filter 
+    /*
+    $("li").filter(function (index) {
+
+        return index % 3 === 2;
+
+    }).css("color", "blue");
+
+    */
+
+
+
+
+    // Another filter example, note the even filter will select odd numbers (index starts at 0)
+    //$("li").filter(":even").css("color", "blue");
+
+
+
+
+    // .is()
+
+    $("li").each(function () {
+
+        var $this = $(this);
+        if ($this.is(":even"))
+            $this.css("color", "green").append("&nbsp;&nbsp; Nice one!");
+
+    });
+
+
+});
